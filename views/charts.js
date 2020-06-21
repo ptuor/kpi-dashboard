82d6f7b6f764be930392c612564909f1e8c89074
Hubbacca <54737443+Hubbacca@users.noreply.github.com>
2020-06-21 16:03:37 +0200

    Implemented basic trend functionalities

--- views/charts.js
+++ views/charts.js
@@ -1,4 +1,8 @@
-let count = 0;
+let count = 0
+let oeeArray = []
+let avaArray = []
+let effArray = []
+let quaArray = []
 
 // Polling request to server
 const sleep = time => new Promise(resolve => setTimeout(resolve, time))
@@ -12,16 +16,23 @@ poll(() => new Promise(() => {
         return response.json()
     }).then(result => {
         console.log(result)
+        oeeArray.push(result.oee)
+        avaArray.push(result.ava)
+        effArray.push(result.eff)
+        quaArray.push(result.qua)
+
         if (count === 0) {
             createChart("oee", result.oee)
             createChart("ava", result.ava)
             createChart("eff", result.eff)
             createChart("qua", result.qua)
+            createTrend(result.oee, result.ava, result.eff, result.qua)
         } else {
             updateChart("oee", 0, result.oee)
             updateChart("ava", 1, result.ava)
             updateChart("eff", 2, result.eff)
             updateChart("qua", 3, result.qua)
+            updateTrend(oeeArray, avaArray, effArray, quaArray)
         }
         count++
     })
@@ -79,6 +90,72 @@ function createChart(name, value) {
     });
 }
 
+// create Trend
+function createTrend(oeeC, avaC, effC, quaC) {
+    Highcharts.chart('trend', {
+
+        yAxis: {
+            title: {
+                text: 'Value in percent [%]'
+            }
+        },
+        xAxis: {
+            title: {
+                text: 'Time in seconds [s]'
+            },
+            accessibility: {
+                rangeDescription: 'Range: 5 to 10'
+            }
+        },
+        legend: {
+            layout: 'vertical',
+            align: 'right',
+            verticalAlign: 'middle'
+        },
+        plotOptions: {
+            series: {
+                label: {
+                    connectorAllowed: false
+                },
+                pointStart: 0,
+                pointInterval: 5
+            }
+        },
+        series: [{
+            name: 'OEE',
+            data: [oeeC],
+            color: "#80ff80"
+        }, {
+            name: 'AVA',
+            data: [avaC],
+            color: "#ffff33"
+        }, {
+            name: 'EFF',
+            data: [effC],
+            color: "#ff9933"
+        }, {
+            name: 'QUA',
+            data: [quaC],
+            color: "#ff0000"
+
+        }],
+        responsive: {
+            rules: [{
+                condition: {
+                    maxWidth: 500
+                },
+                chartOptions: {
+                    legend: {
+                        layout: 'horizontal',
+                        align: 'center',
+                        verticalAlign: 'bottom'
+                    }
+                }
+            }]
+        }
+    });
+}
+
 //update charts
 function updateChart(name, num, val) {
     Highcharts.charts[num].series[0].setData([{
@@ -88,4 +165,24 @@ function updateChart(name, num, val) {
     },
         ["??", (100 - val)]
     ])
+}
+
+//update trend
+function updateTrend(oeeT, avaT, effT, quaT) {
+    let values
+    for (let i = 0; i <= 3; i++) {
+        if (i === 0) {
+            values = oeeT
+        } else if (i === 1) {
+            values = avaT
+        } else if (i === 2) {
+            values = effT
+        } else if (i === 3) {
+            values = quaT
+        }
+        Highcharts.charts[4].series[i].setData(
+            values
+        )
+    }
+
 }
