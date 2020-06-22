406a889fa1c342eec9a6c74d4b72c5f31de099bf
Hubbacca <54737443+Hubbacca@users.noreply.github.com>
2020-06-22 22:51:47 +0200

    bugfix datepicker

--- views/history/app.js
+++ views/history/app.js
@@ -1,7 +1,7 @@
 import {View} from'./view.js'
 import { Controller } from './controller.js'
 
-const view = new View("#config")
+const view = new View("#config1")
 const controller = new Controller(view)
 
 controller.start()
